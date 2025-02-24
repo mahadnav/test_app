396e213781d61c874be52affc9790d13f998509a
mahadnav <mahadn20@gmail.com>
2025-02-24 18:34:12 +0500

    ''

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ if uploaded_file is not None:
         # Customizing the appearance
         ax.set_yticks(np.arange(len(pm2_5_matrix.index)))
         ax.set_yticklabels(pm2_5_matrix.index, color='white', fontsize=24)
-        ax.set_xticks([])  # Remove xticks
+        ax.set_xticks([]) 
         ax.set_xlabel("")
         # ax.set_ylabel("Year", color='white')
         fig.patch.set_alpha(0)  # Transparent background
@@ -110,8 +110,20 @@ if uploaded_file is not None:
         ##################### new section
         st.write("#### Comparative Analysis")
         city_avg_pm25 = df.pivot_table(index = 'datetime', columns='City', values='PM2.5', aggfunc='mean')
-        selected_cities = st.multiselect("Select Cities", options=list(df.City.unique()), default=None)
+        selected_cities = st.multiselect("Select Cities", options=list(df['City'].unique()), default=['Lahore', 'Karachi'])
         city_avg_pm25 = city_avg_pm25[selected_cities]
+
+        # daily, monthly, annually
+        selected_trend = st.select("Select Time Trend", options=['Daily', 'Monthly', 'Annually'], default=['Daily'])
+
+        if selected_trend == 'Daily':
+            city_avg_pm25 = city_avg_pm25.resample('1D').mean().sort_index()
+        elif selected_trend == 'Monthly':
+            city_avg_pm25 = city_avg_pm25.resample('1M').mean().sort_index()
+        else:
+            city_avg_pm25 = city_avg_pm25.resample('1Y').mean().sort_index()
+
+
         city_trends = px.line(city_avg_pm25, x=city_avg_pm25.index, y=selected_cities)
         st.plotly_chart(city_trends)
 
