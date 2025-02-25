02081945c0c05eee187f375457ab445ab28f39c0
mahadnav <mahadn20@gmail.com>
2025-02-25 17:05:59 +0500

    ''

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with centered_col[1]:
     '''
     # :earth_americas: PM2.5 Data Analysis & Visualization
 
-    This tool allows you to upload your data set and analyze it in just a few seconds!
+    \tThis tool allows you to upload your data set and analyze it in just a few seconds!
     '''
 
     ''
