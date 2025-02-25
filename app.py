46bffa75aed93792b399dbcde2bcf390279964ad
mahadnav <mahadn20@gmail.com>
2025-02-25 16:19:03 +0500

    ''

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ with centered_col[1]:
                 ).add_to(marker_cluster)
             
             marker_cluster.add_to(m)
-            folium_static(m, width=1220, height=800)
+            folium_static(m, width=1220, height=700)
 
 
 
