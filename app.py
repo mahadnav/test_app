6ad06e52e0c9a1d06149e8f4d9bcefe5a0345913
mahadnav <mahadn20@gmail.com>
2025-02-26 15:07:10 +0500

    ''

--- app.py
+++ app.py
@@ -171,7 +171,7 @@ with centered_col[1]:
             def create_sparkline(data, color):
                 fig = go.Figure()
                 fig.add_trace(go.Scatter(y=data, mode="lines", line=dict(color=color, width=1)))
-                fig.update_layout(margin=dict(l=0, r=0, t=0, b=0), xaxis=dict(visible=False), yaxis=dict(visible=False), height=50, width=2)
+                fig.update_layout(margin=dict(l=1, r=1, t=0, b=0), xaxis=dict(visible=False), yaxis=dict(visible=False), height=50)
                 return fig
 
             col1, col2, col3 = st.columns(3)
