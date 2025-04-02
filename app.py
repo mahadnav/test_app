fb893cdba4a7e4151dd471cfd4a1721c47eef26d
mahadnav <mahadn20@gmail.com>
2025-04-03 01:24:46 +0500

    ''

--- app.py
+++ app.py
@@ -314,6 +314,7 @@ with centered_col[1]:
             cbar = fig.colorbar(cax, boundaries=breakpoints, orientation='horizontal', shrink=0.9, pad=0.01)
             cbar.set_ticks(midpoints)
             cbar.set_ticklabels(categories[:-1], fontsize=16)
+            cbar.ax.set_aspect(0.2)
             cbar.ax.tick_params(axis='x', colors='white', labelsize=16, length=0, pad=10)
             
             fig.patch.set_alpha(0)
